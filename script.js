33240269c655d7121bfbfb98eaea053997d9a031
Swntoz <131720514+Swntoz@users.noreply.github.com>
2023-06-12 22:05:47 -0300

    script.js

--- script.js
+++ script.js
@@ -1,78 +1,159 @@
-let tabuleiroHTML = document.querySelector('#tabuleiro')
+
+let posicao0 = document.querySelector("#posicao0")
+let posicao1 = document.querySelector("#posicao1")
+let posicao2 = document.querySelector("#posicao2")
+let posicao3 = document.querySelector("#posicao3")
+let posicao4 = document.querySelector("#posicao4")
+let posicao5 = document.querySelector("#posicao5")
+let posicao6 = document.querySelector("#posicao6")
+let posicao7 = document.querySelector("#posicao7")
+let posicao8 = document.querySelector("#posicao8")
 
 let tabuleiro = ['_', '_', '_', '_', '_', '_', '_', '_', '_']
-let jogada = 0, posicao
+let jogada = 0, posicao, jogador
 
-//Variáveis lógicas para verificar se há campeão:
-//verdadeiro -> todos valores são X ou O
-let linhaA, linhaB, linhaC, colunaD, colunaE, colunaF, diagonalG, diagonalH
+//Verifica se é a vez do X ou O:
+function atribuirJogador(){
+    //Verifica se é a vez do X ou O:
+  //Se 'jogada' é par então é a vez do X
+  
+  if(jogada % 2 === 0){
+    jogador = 'X'  
+  }
+  //Se 'jogada' é impar então é a vez do O
+  else{
+    jogador = 'O'
+  }
+  return jogador
+}
 
-while (jogada <= 8) {
-  //Impressão do tabuleiro
-  tabuleiroHTML.innerHTML = 
-`${tabuleiro[0]}|${tabuleiro[1]}|${tabuleiro[2]}<br>
- ${tabuleiro[3]}|${tabuleiro[4]}|${tabuleiro[5]}<br>
- ${tabuleiro[6]}|${tabuleiro[7]}|${tabuleiro[8]}`
-  //Jogada do X
-  //Repete a jogada se a jogada é inválida:
-  while (true) {
-    posicao = Number(prompt("Jogada do X (digite uma posição de 0 a 8): "))
-    //Se a posição é válida então aplica a jogada no tabuleiro e para o laço infinito:
-    if (posicao >= 0 && posicao <= 8 && tabuleiro[posicao] === '_') {
-      tabuleiro[posicao] = 'X'
-      jogada++
-      break
+//Verifica se a posição jogada está vazia e realiza a jogada:
+function realizarJogada(posicao, botao, jogador){
+  if(tabuleiro[posicao] === '_'){
+    botao.innerHTML = jogador
+    tabuleiro[posicao] = jogador
+    jogada++
     }
-  }
-  tabuleiroHTML.innerHTML = 
-`${tabuleiro[0]}|${tabuleiro[1]}|${tabuleiro[2]}<br>
- ${tabuleiro[3]}|${tabuleiro[4]}|${tabuleiro[5]}<br>
- ${tabuleiro[6]}|${tabuleiro[7]}|${tabuleiro[8]}`
+}
 
-  //Verificação se X é vencedor em uma das linhas, colunas ou diagonais:
-  linhaA = (tabuleiro[0] === 'X') && (tabuleiro[1] === 'X') && (tabuleiro[2] === 'X')
-  linhaB = (tabuleiro[3] === 'X') && (tabuleiro[4] === 'X') && (tabuleiro[5] === 'X')
-  linhaC = (tabuleiro[6] === 'X') && (tabuleiro[7] === 'X') && (tabuleiro[8] === 'X')
-  colunaD = (tabuleiro[0] === 'X') && (tabuleiro[3] === 'X') && (tabuleiro[6] === 'X')
-  colunaD = (tabuleiro[1] === 'X') && (tabuleiro[4] === 'X') && (tabuleiro[7] === 'X')
-  colunaD = (tabuleiro[2] === 'X') && (tabuleiro[5] === 'X') && (tabuleiro[8] === 'X')
-  diagonalG = (tabuleiro[0] === 'X') && (tabuleiro[4] === 'X') && (tabuleiro[8] === 'X')
-  diagonalH = (tabuleiro[2] === 'X') && (tabuleiro[4] === 'X') && (tabuleiro[6] === 'X')
+function resetarTabuleiro(){
 
-  //Condicional para saber se "X" é o vencedor:
-  if(linhaA || linhaB || linhaC || colunaD || colunaE || colunaF || diagonalG || diagonalH){
-    alert(`O Jogador X é o vencedor!!!`)
-    break
-  }
+}
 
+function verificarCampeao(jogador){
+  //Verificação se X ou O é vencedor em uma das linhas, colunas ou diagonais:
+  linhaA = (tabuleiro[0] === jogador) && (tabuleiro[1] === jogador) && (tabuleiro[2] === jogador)
+  linhaB = (tabuleiro[3] === jogador) && (tabuleiro[4] === jogador) && (tabuleiro[5] === jogador)
+  linhaC = (tabuleiro[6] === jogador) && (tabuleiro[7] === jogador) && (tabuleiro[8] === jogador)
+  colunaD = (tabuleiro[0] === jogador) && (tabuleiro[3] === jogador) && (tabuleiro[6] === jogador)
+  colunaE = (tabuleiro[1] === jogador) && (tabuleiro[4] === jogador) && (tabuleiro[7] === jogador)
+  colunaF = (tabuleiro[2] === jogador) && (tabuleiro[5] === jogador) && (tabuleiro[8] === jogador)
+  diagonalG = (tabuleiro[0] === jogador) && (tabuleiro[4] === jogador) && (tabuleiro[8] === jogador)
+  diagonalH = (tabuleiro[2] === jogador) && (tabuleiro[4] === jogador) && (tabuleiro[6] === jogador)
 
-
-  if (jogada <= 8) {
-    //Jogada do O
-    //Repete a jogada se a jogada é inválida:
-    while (true) {
-      posicao = Number(prompt("Jogada do O (digite uma posição de 0 a 8): "))
-      //Se a posição é válida então aplica a jogada no tabuleiro e para o laço infinito:
-      if (posicao >= 0 && posicao <= 8 && tabuleiro[posicao] === '_') {
-        tabuleiro[posicao] = 'O'
-        jogada++
-        break
-      }
-    }
-      //Verificação se O é vencedor em uma das linhas, colunas ou diagonais:
-  linhaA = (tabuleiro[0] === 'O') && (tabuleiro[1] === 'O') && (tabuleiro[2] === 'O')
-  linhaB = (tabuleiro[3] === 'O') && (tabuleiro[4] === 'O') && (tabuleiro[5] === 'O')
-  linhaC = (tabuleiro[6] === 'O') && (tabuleiro[7] === 'O') && (tabuleiro[8] === 'O')
-  colunaD = (tabuleiro[0] === 'O') && (tabuleiro[3] === 'O') && (tabuleiro[6] === 'O')
-  colunaD = (tabuleiro[1] === 'O') && (tabuleiro[4] === 'O') && (tabuleiro[7] === 'O')
-  colunaD = (tabuleiro[2] === 'O') && (tabuleiro[5] === 'O') && (tabuleiro[8] === 'O')
-  diagonalG = (tabuleiro[0] === 'O') && (tabuleiro[4] === 'O') && (tabuleiro[8] === 'O')
-  diagonalH = (tabuleiro[2] === 'O') && (tabuleiro[4] === 'O') && (tabuleiro[6] === 'O')
-
-  //Condicional para saber se "O" é o vencedor
-  if(linhaA || linhaB || linhaC || colunaD || colunaE || colunaF || diagonalG || diagonalH){
-    alert(`O Jogador O é o vencedor!!!`)
-    break
-  }
+  //Condicional de teste para saber se X é vencedor:
+  if (linhaA || linhaB || linhaC || colunaD || colunaE || colunaF || diagonalG || diagonalH) {
+    alert(`Parabéns!!! ${jogador} venceu!`)
+    //resetarTabuleiro()
   }
 }
+
+posicao0.addEventListener("click", function() {
+
+  jogador = atribuirJogador()
+
+  realizarJogada(0, posicao0, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao1.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(1, posicao1, jogador)
+  
+  verificarCampeao(jogador)
+  
+})
+
+posicao2.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(2, posicao2, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao3.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(3, posicao3, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao4.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(4, posicao4, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao5.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(5, posicao5, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao6.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(6, posicao6, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao7.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(7, posicao7, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao8.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(8, posicao8, jogador)
+
+  verificarCampeao(jogador)
+  
+})
+
+posicao9.addEventListener("click", function(){
+
+  jogador = atribuirJogador()
+
+  realizarJogada(9, posicao9, jogador)
+
+  verificarCampeao(jogador)
+  
+})
